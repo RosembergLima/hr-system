b5ace63d6af5b4b7ce10f1aa0843606102bd02f2
Rosemberg Bezerra <r.bezerra@kigroup.de>
2023-01-24 17:39:40 +0000

    adding eureka

--- user-api/user-api/src/main/java/com/hr/system/userapi/services/impl/UserServiceImpl.java
+++ user-api/user-api/src/main/java/com/hr/system/userapi/services/impl/UserServiceImpl.java
@@ -7,18 +7,22 @@ import com.hr.system.userapi.services.exceptions.ObjectNotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-
+    private final Environment env;
     @Override
     public User findById(Long id) {
+        log.info("User Service ::: Get request on " + env.getProperty("local.server.port") + " port");
         return userRepository.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundException("User not found"));
     }
